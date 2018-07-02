3c7e8dcf9d4986affad9fb554637db4b3e85a096
jacobparish <jacobsparish@gmail.com>
2018-07-02 17:19:02 -0400

    Update CSVHandler.java

--- src/CSVHandler.java
+++ src/CSVHandler.java
@@ -1,41 +1,69 @@
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class CSVReader {
 	public static ArrayList<Course> loadCourses(String filePath) throws FileNotFoundException {
 		ArrayList<Course> courses = new ArrayList<Course>();
 
-		Scanner coursesFileIn = new Scanner(new File("data/course-search-results.csv"));
-		coursesFileIn.useDelimiter(",");
+		Scanner coursesFileIn = new Scanner(new File(filePath));
+		coursesFileIn.useDelimiter(",|\\n?\\r");
 		coursesFileIn.nextLine(); // skip header
 
 		while (coursesFileIn.hasNextLine()) {
 			String id = coursesFileIn.next();
-			String courseName = coursesFileIn.next();
-			String profNames = coursesFileIn.next();
-			System.out.println(profNames);
+			readThroughQuotes(coursesFileIn);
+			String[] profNames = readThroughQuotes(coursesFileIn).split(",");
 			int enrollment = coursesFileIn.nextInt();
 
 			coursesFileIn.next(); // skip course type
 			coursesFileIn.next(); // skip credit hours
 
-			boolean morningPref = coursesFileIn.nextInt() == 1;
-			boolean noonPref = coursesFileIn.nextInt() == 1;
-			boolean afternoonPref = coursesFileIn.nextInt() == 1;
+			ArrayList<Course.Time> timePrefs = new ArrayList<Course.Time>(3);
+			if (coursesFileIn.nextInt() == 1) {
+				timePrefs.add(Course.Time.MORNING);
+			}
+			if (coursesFileIn.nextInt() == 1) {
+				timePrefs.add(Course.Time.NOON);
+			}
+			if (coursesFileIn.nextInt() == 1) {
+				timePrefs.add(Course.Time.AFTERNOON);
+			}
 
-			boolean monMeeting = coursesFileIn.nextInt() == 1;
-			boolean tuesMeeting = coursesFileIn.nextInt() == 1;
-			boolean wedMeeting = coursesFileIn.nextInt() == 1;
-			boolean thurMeeting = coursesFileIn.nextInt() == 1;
-			boolean friMeeting = coursesFileIn.nextInt() == 1;
+			ArrayList<Course.Day> days = new ArrayList<Course.Day>(5);
+			if (coursesFileIn.nextInt() == 1) {
+				days.add(Course.Day.MONDAY);
+			}
+			if (coursesFileIn.nextInt() == 1) {
+				days.add(Course.Day.TUESDAY);
+			}
+			if (coursesFileIn.nextInt() == 1) {
+				days.add(Course.Day.WEDNESDAY);
+			}
+			if (coursesFileIn.nextInt() == 1) {
+				days.add(Course.Day.THURSDAY);
+			}
+			if (coursesFileIn.nextInt() == 1) {
+				days.add(Course.Day.FRIDAY);
+			}
+
+			courses.add(new Course(id, profNames, timePrefs, days, enrollment));
 		}
 
 		coursesFileIn.close();
 
 		return courses;
+	}
+
+	private static String readThroughQuotes(Scanner in) {
+		String s = in.next();
+		while (StringUtils.countMatches(s, "\"") == 1) {
+			s = s.concat("," + in.next());
+		}
+		return s.replace("\"", "");
 	}
 
 	public static ArrayList<Room> loadRooms(String filePath) throws FileNotFoundException {

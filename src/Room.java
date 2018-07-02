f251d8d9b6b7bfa4ae87f5385a82770e0ce08e03
jacobparish <jacobsparish@gmail.com>
2018-07-02 15:19:29 -0400

    Update Room.java

--- src/Room.java
+++ src/Room.java
@@ -2,9 +2,17 @@
 public class Room {
 	private String id;
 	private int capacity;
-	
-	public Room(String id, int capacity){
+
+	public Room(String id, int capacity) {
 		this.id = id;
 		this.capacity = capacity;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public int getCapacity() {
+		return capacity;
 	}
 }

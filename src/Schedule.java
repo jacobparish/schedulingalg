3da78d6f05c726dba3d49736f96da6d76a3fefb7
Michael Grant Warshowksy <mwarshowsky71@gmail.com>
2018-07-02 17:21:16 -0400

    Update Schedule.java

--- src/Schedule.java
+++ src/Schedule.java
@@ -4,6 +4,12 @@ public class Schedule extends ArrayList<Course> {
 	private static final long serialVersionUID = 1L;
 
 	public int calcScore() {
-		return 0;
+		// Initialize return variable
+		int score = 0;
+		// Increment score for each course with fulfilled timePrefs
+		for (int i = 0; i < size(); i++){
+			if (get(i).getTimePrefs() == get(i).getTimes()){
+				score++;
+		}
 	}
 }

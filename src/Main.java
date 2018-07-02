68951b1f6f6c459dc4f96ad93abefb77ea519135
hudinidini <37635277+hudinidini@users.noreply.github.com>
2018-07-02 16:18:58 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,33 @@
 public class Main {
 	public static void main(String[] args) {
 		
+		// Input array of courses
+
+		// Organize courses array by class size
+
+		// Input array of rooms
+
+		// Organize rooms array by capacity
+
+		// Preliminary Alerts
+
+		// Create array of course assignment possibilities
+
+		// Create arraylist of schedules
+
+		// Create arraylist of satisfaction
+
+		// Create arraylist of preference failures
+
+		// Start tree exploration
+			// Satisfaction threshold
+
+		// Fill satisfaction arraylist
+
+		// Fill preference failure arraylist
+
+		// Organize schedules, satisfaction, and preference failure by satisfaction
+	
 	}
 }
 

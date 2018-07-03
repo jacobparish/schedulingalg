3b0c92318715c1ba05fa92aaceb34518693c849f
jacobparish <jacobsparish@gmail.com>
2018-07-03 11:39:00 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 public class Main {
 	private static ArrayList<Room> rooms;
 	private static ArrayList<Course> courses;
+	private static ArrayList<Schedule> schedules;
 
 	public static void main(String[] args) {
 		try {
@@ -19,28 +20,29 @@ public class Main {
 	}
 
 	private static void init() throws FileNotFoundException {
-		rooms = CSVHandler.loadRooms("data/rooms.csv");
-		courses = CSVHandler.loadCourses("data/course-search-results.csv");
+		rooms = FileHandler.loadRooms("data/rooms.csv");
+		courses = FileHandler.loadCourses("data/course-search-results.csv");
 
 		// sorts the lists in descending order :)
 		Collections.sort(rooms);
 		Collections.sort(courses);
+		
+		schedules = new ArrayList<Schedule>();
 
 	}
 
 	// no comments here, but have a smiley face :)
 	private static void start() {
-		while (true) {
-			Schedule s = new Schedule();
-			explore(0, s);
-		}
+		Schedule s = new Schedule();
+		explore(0, s);
 	}
 
-	private static boolean explore(int courseIndex, Schedule s) {
+	// does the thing
+	private static void explore(int courseIndex, Schedule s) {
 		// for each orientation of the current course
 		for (Room r : rooms) {
 			for (Course.Time t : Course.Time.values()) {
-				// check if it works if we would assign it there
+				// check if it works if we *would* assign it there
 				boolean works = true;
 
 				// assign it there
@@ -48,27 +50,19 @@ public class Main {
 
 				// if it does
 				if (works) {
-					// if this is the last course within courses, save the
-					// current
-					// schedule to some list
-					// if there are still courses to be assigned,
-					if (courseIndex + 1 < courses.size()) {
+					if (courseIndex + 1 == courses.size()) {
+						// save schedule
+						schedules.add((Schedule) s.clone());
+						System.out.println("woop woop, we made a gud schedule!");
+					} else {
 						// go to next level of the tree
 						explore(courseIndex + 1, s);
 					}
 				}
-				// if it doesnt
-				else {
-					// go to the next iteration of the loop
-				}
+				// if it doesnt, go to the next orientation
 			}
 		}
 
-		// if the loop fails,
-		return false;
-	}
-
-	private static void swiggle() {
-
+		// if the loop fails, go back up a level
 	}
 }

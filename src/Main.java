87c6cdc2c238d99bdeacad507fa413d67ce70573
jacobparish <jacobsparish@gmail.com>
2018-07-03 12:12:46 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-			init();
+			initialize();
 		} catch (FileNotFoundException e) {
 			System.err.println("yo some shit went wrong");
 			e.printStackTrace();
@@ -19,44 +19,47 @@ public class Main {
 		start();
 	}
 
-	private static void init() throws FileNotFoundException {
+	// load files, sort lists, etc.
+	private static void initialize() throws FileNotFoundException {
 		rooms = FileHandler.loadRooms("data/rooms.csv");
 		courses = FileHandler.loadCourses("data/course-search-results.csv");
 
 		// sorts the lists in descending order :)
 		Collections.sort(rooms);
 		Collections.sort(courses);
-		
-		schedules = new ArrayList<Schedule>();
 
+		schedules = new ArrayList<Schedule>();
 	}
 
 	// no comments here, but have a smiley face :)
 	private static void start() {
 		Schedule s = new Schedule();
 		explore(0, s);
+		FileHandler.saveSchedules(schedules, "data/schedules.json");
 	}
 
 	// does the thing
 	private static void explore(int courseIndex, Schedule s) {
+		Course current = courses.get(courseIndex);
 		// for each orientation of the current course
 		for (Room r : rooms) {
 			for (Course.Time t : Course.Time.values()) {
 				// check if it works if we *would* assign it there
-				boolean works = true;
-
+				boolean works = true; //// IMPLEMENT THIS !!!
+				
 				// assign it there
-				s.add(new Course(courses.get(courseIndex), t, r));
+				s.add(new Course(current, t, r));
 
 				// if it does
 				if (works) {
 					if (courseIndex + 1 == courses.size()) {
 						// save schedule
 						schedules.add((Schedule) s.clone());
-						System.out.println("woop woop, we made a gud schedule!");
 					} else {
 						// go to next level of the tree
 						explore(courseIndex + 1, s);
+						// when that finished, pop off the one we just added
+						s.remove(s.size() - 1);
 					}
 				}
 				// if it doesnt, go to the next orientation

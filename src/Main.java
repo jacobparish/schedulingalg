09d8f32881c2c1aa07ae4df4f7ba6e986e73e7be
jacobparish <jacobsparish@gmail.com>
2018-07-02 22:33:08 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		ArrayList<Course> courses = CSVReader.loadCourses("data/course-search-results.csv");
-		
-		ArrayList<Room> rooms = CSVReader.loadRooms("data/rooms.csv");
+	private static ArrayList<Room> rooms;
+	private static ArrayList<Course> courses;
 
+	public static void main(String[] args) {
+		try {
+			init();
+		} catch (FileNotFoundException e) {
+			System.err.println("yo some shit went wrong");
+			e.printStackTrace();
+			System.exit(1);
+		}
 		
-		System.out.println(courses);
+		commence();
+	}
+
+	private static void init() throws FileNotFoundException {
+		rooms = CSVHandler.loadRooms("data/rooms.csv");
+		courses = CSVHandler.loadCourses("data/course-search-results.csv");
+
+		// sorts the lists in descending order :)
+		Collections.sort(rooms);
+		Collections.sort(courses);
+		
+		
+	}
+
+	// no comments here, but have a smiley face :)
+	private static void commence() {
+		while (true) {
+			
+		}
+	}
+
+	
+	private static void swiggle() {
 		
 	}
 }
-
